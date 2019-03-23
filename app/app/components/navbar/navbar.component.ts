67f02b7809731bcd7a2d6bccd9e58b7c767db958
Archana Potnis <potnis.archana@gmail.com>
2019-03-22 17:58:10 -0700

    Add files via upload

---
+++ app/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by archie on 11/16/16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    moduleId: module.id,
+    selector: 'navbar',
+    templateUrl : 'navbar.component.html'
+})
+export class NavbarComponent {
+    branding = 'MyWebsite';
+}
---
+++ app/app/components/pages/home.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by archie on 11/16/16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    moduleId: module.id,
+    selector: 'home',
+    templateUrl : 'home.component.html'
+})
+export class HomeComponent {
+}
